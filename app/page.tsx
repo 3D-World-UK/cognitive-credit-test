9c03000dae219dc2a20c646bcfe7783c3216c441
3enny3oy <82097645+3DWorldUK@users.noreply.github.com>
2022-12-09 17:34:35 +0000

    fixed page type error

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 import { Children } from "react";
 
-export default function Page({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
+export default function Page() {
     return (
     <div className="flex min-h-screen flex-col items-center justify-center">
     <header className="flex w-full h-24 items-center justify-center border-b dark:border-b-slate-900 bg-sky-200 dark:bg-slate-700 dark:text-white drop-shadow-md"><h1>Cognitive Credit - Report Schedule</h1></header>
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center bg-sky-100 dark:bg-slate-600 dark:text-white">
-        {children}
+        
       </main>
       <footer className="flex h-24 w-full items-center justify-center border-t  dark:border-t-slate-900 bg-sky-200 dark:bg-slate-700 dark:text-white drop-shadow-md">
         <a
