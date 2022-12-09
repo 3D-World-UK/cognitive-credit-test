5651b41c60f078a284601f8e25db350c0ea4a0e6
3enny3oy <82097645+3DWorldUK@users.noreply.github.com>
2022-12-09 18:20:01 +0000

    Rolled back Next.JS 13 experimental features. they aren't fully supported for static export yet

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
-import { Children } from "react";
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
 
-export default function Page() {
-    return (
+const Home: NextPage = () => {
+  return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-    <header className="flex w-full h-24 items-center justify-center border-b dark:border-b-slate-900 bg-sky-200 dark:bg-slate-700 dark:text-white drop-shadow-md"><h1>Cognitive Credit - Report Schedule</h1></header>
+      <Head>
+        <title>Ben Knowles - Cognitive Credit Technical Test</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="robots" content="noindex" />
+      </Head>
+      <header className="flex w-full h-24 items-center justify-center border-b dark:border-b-slate-900 bg-sky-200 dark:bg-slate-700 dark:text-white drop-shadow-md"><h1>Cognitive Credit - Report Schedule</h1></header>
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center bg-sky-100 dark:bg-slate-600 dark:text-white">
-        
+
       </main>
       <footer className="flex h-24 w-full items-center justify-center border-t  dark:border-t-slate-900 bg-sky-200 dark:bg-slate-700 dark:text-white drop-shadow-md">
         <a
@@ -18,5 +26,7 @@ export default function Page() {
         </a>
       </footer>
     </div>
-    )
+  )
 }
+
+export default Home
